146980a1935bea9db170dcd712cb5c8172948855
Andrés Felipe Trujillo <andres.trujillo.1@gmail.com>
2026-09-15 14:22:03 +0200

    add templates

--- compassMap/src/main/java/com/example/demo/controller/DemoController.java
+++ compassMap/src/main/java/com/example/demo/controller/DemoController.java
@@ -1,15 +1,15 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DemoController {
 
-    @RequestMapping("/")
-    @ResponseBody
+    @GetMapping("/")
     public String home() {
-        return "<h1>home</h1>";
+        return "home";
     }
 }
